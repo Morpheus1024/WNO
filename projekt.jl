5ddc878597f640760d503a9b751f378c8a8d90a9
Mikołaj Galant <mikolaj.galant@gmail.com>
2023-10-29 00:06:07 +0200

    Update projekt.jl

--- projekt.jl
+++ projekt.jl
@@ -8,7 +8,7 @@ const d = 7900    # średnia gęstość stali nierdzewnej
 const Cw = 500    # średnie ciepło właściwe sali nierdzewnej
 const h_O2 = 280  # współczynnik konwekcji dla przepływu powietrza
 
-
+using Plots
 
 # pobieranie zmiennych symulacyjnych od użytkonika
 print("Temperatura otoczenia [C]: ")
@@ -31,13 +31,13 @@ temp_na_danymm_odc_trasy = []
 
 
 delta_x = 0.002 #iterowanie co 2 milimetry ostrza
-delta_s = 0.1 # iterowanie co 10 cm trasy
+delta_s = 0.01 # iterowanie co 1 cm trasy
 przebyta_droga = 0
 dlugosc_na_ostrzu = 0
 T = T0 # warunek początkowy
 for i in 1:s*100
     global T = T0
-    for j in 0:x*100*10
+    for j in 0:x*100*5
         global dlugosc_na_ostrzu+=delta_x
         delta_temp = (u*m*g*v - h*h_O2*(2*dlugosc_na_ostrzu+2*a)*(T-T0))/(a*dlugosc_na_ostrzu*(d*Cw*v*h/przebyta_droga + k/delta_x))
         #print(delta_temp)
@@ -47,9 +47,13 @@ for i in 1:s*100
     global przebyta_droga += delta_s
     global dlugosc_na_ostrzu =0
     push!(temp, temp_na_danymm_odc_trasy)
+    #savefig(plot(x, temp_na_danymm_odc_trasy),"plot.pdf")
     global temp_na_danymm_odc_trasy =[]
 end
-print(temp)
+print(T)
+
+# plot3d(temp)
 #print(temp_na_danymm_odc_trasy)
 #end
+
 
