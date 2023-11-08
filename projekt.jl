0e5f34c0f1a15f53c47d7db447c970339078955c
Mikołaj Galant <mikolaj.galant@gmail.com>
2023-11-08 22:18:34 +0100

    Update projekt.jl

--- projekt.jl
+++ projekt.jl
@@ -26,10 +26,10 @@ using Plots
 # print("Prędkość z jaką porusza się łyżwiarz [m/s]: ")
 # v = parse(Float32, readline())
 
-T0 = -20
-m = 80
+T0 = -30
+m = 100
 x = 250
-s = 1
+s = 10
 v = 1
 
 temp = zeros(Float32, s*100)
@@ -44,9 +44,7 @@ for i in 1:s*100
     if i==1
         global T = T0
         global temp[i] = T0
-        print(i)
-        print(". ")
-        println(temp[i])
+
     else
         global przebyta_droga +=delta_s
         delta_temp = (alfa*d*a*H*x*Cw*(T+T_poprzednie)+delta_x*delta_x*(u*m*g*v-h*x*H*(T-T0)-epsilon*sigma*(T*T*T*T-T0*T0*T0*T0)))/(alfa*d*a*H*x*Cw + delta_x*k*x*H)
@@ -54,13 +52,11 @@ for i in 1:s*100
         global T -=delta_temp
             
         global temp[i] = T
-        print(i)
-        print(". ")
-        println(temp[i])
+        
     end
 end
 
-println(temp[s*90])
+#println(temp[s*90])
 #plot(temp, shape(temp))
 # Generowanie siatki dla wykresu 3D
 # m,n = temp.size
@@ -77,4 +73,9 @@ println(temp[s*90])
 #Plots.pdf(plot,"projekt")
 
 # Tworzenie wykresu 3D
+
+ox = repeat(1:s*100)
+
+p = plot(ox/100, temp[ox])
+Plots.pdf(p,"plot")
 
