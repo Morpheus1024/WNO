67f5432a0ca838e0c83389d63cc3ebb044f2be96
Mikołaj Galant <mikolaj.galant@gmail.com>
2023-12-04 21:41:53 +0100

    Update test.py

--- Laboratorium 2 - pakowanie prezentów/Lab 2/test.py
+++ Laboratorium 2 - pakowanie prezentów/Lab 2/test.py
@@ -28,10 +28,6 @@ def convex_hull_animation(canvas, points, hull_indices, i, connecting_lines):
         first_point = points[first_index]
         connecting_line = canvas.create_line(last_point.x, last_point.y, first_point.x, first_point.y, fill="red")
         connecting_lines.append(connecting_line)
-
-        # Print connected points in the terminal
-        connected_points = [points[index] for index in hull_indices]
-        print(f"Connected Points: {[str(point) for point in connected_points]}")
         return
 
     current_index = hull_indices[i]
@@ -46,6 +42,10 @@ def convex_hull_animation(canvas, points, hull_indices, i, connecting_lines):
         previous_point = points[previous_index]
         connecting_line = canvas.create_line(previous_point.x, previous_point.y, current_point.x, current_point.y, fill="red")
         connecting_lines.append(connecting_line)
+
+    # Print coordinates in the terminal
+    print(f"Wrapped Points: {[str(points[index]) for index in hull_indices[:i+1]]}")
+    print()
 
     canvas.after(500, convex_hull_animation, canvas, points, hull_indices, i + 1, connecting_lines)
 
@@ -91,13 +91,6 @@ def main():
     # Draw convex hull points
     for index in hull_indices:
         points[index].draw(color="red")
-
-    # Connect the convex hull points before starting the animation
-    for i in range(len(hull_indices) - 1):
-        p1 = points[hull_indices[i]]
-        p2 = points[hull_indices[i + 1]]
-        print(f"Connecting: {p1} to {p2}")
-        canvas.create_line(p1.x, p1.y, p2.x, p2.y, fill="red")
 
     # Wait for a moment before starting the animation
     root.after(2000, convex_hull_animation, canvas, points, hull_indices, 0, [])
